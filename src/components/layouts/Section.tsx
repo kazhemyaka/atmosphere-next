89a47dd78a4931321a943f5e64dcce03fdbe4e8b
Mykyta Opanasiuk <nikitaopanassiuk@outlook.com>
2024-08-08 13:57:40 +0300

    Remove unnecessary code in Section

--- src/components/layouts/Section.tsx
+++ src/components/layouts/Section.tsx
@@ -3,24 +3,11 @@ import { cn } from "@/utils/cn";
 
 interface SectionProps extends PropsWithChildren {
   className?: string;
-  onMouseMove?: any;
-  ref?: any;
 }
 
-const Section: FC<SectionProps> = ({
-  children,
-  className,
-  onMouseMove,
-  ref,
-}) => {
+const Section: FC<SectionProps> = ({ children, className }) => {
   return (
-    <section
-      className={cn("px-5 sm:px-10", className)}
-      onMouseMove={onMouseMove}
-      ref={ref}
-    >
-      {children}
-    </section>
+    <section className={cn("px-5 sm:px-10", className)}>{children}</section>
   );
 };
 
