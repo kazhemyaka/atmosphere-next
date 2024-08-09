09b5ede39453b50ff3c95229bf19d19a4e13f269
Mykyta Opanasiuk <nikitaopanassiuk@outlook.com>
2024-08-09 11:49:49 +0300

    Added HourlyWeatherCard component

---
+++ src/components/weathercards/HourlyWeatherCard.tsx
@@ -0,0 +1,37 @@
+import { WeatherData } from "@/types";
+import { FC } from "react";
+import DateTime from "@/components/DateTime";
+import WeatherIcon from "@/components/WeatherIcon";
+import TempWithDesc from "@/components/TempWithDesc";
+import { WeatherInfos } from "../weatherinfos";
+import { hourlyWeatherInfos } from "@/data";
+
+interface HourlyWeatherCardProps {
+  hour: WeatherData["forecast"]["forecastday"][0]["hour"][0];
+  timeZone: string;
+}
+
+const HourlyWeatherCard: FC<HourlyWeatherCardProps> = ({ hour, timeZone }) => {
+  return (
+    <div className="rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 overflow-hidden">
+      <DateTime
+        dateTimeEpoch={hour.time_epoch}
+        timeZone={timeZone}
+        hours={true}
+      />
+      <div className="p-6 flex flex-col">
+        <div className="flex items-center mb-4">
+          <WeatherIcon
+            weatherCode={hour.condition.code}
+            isDay={hour.is_day}
+            size={80}
+          />
+          <TempWithDesc temp={hour.temp_c} aftertext={hour.condition.text} />
+        </div>
+        <WeatherInfos weatherInfos={hourlyWeatherInfos(hour)} />
+      </div>
+    </div>
+  );
+};
+
+export default HourlyWeatherCard;
---
+++ src/components/weathercards/index.tsx
@@ -0,0 +1,1 @@
+export { default as HourlyWeatherCard } from "./HourlyWeatherCard";
