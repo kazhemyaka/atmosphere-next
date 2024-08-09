4fe60c88b7eaee3c47cb6f192b93ab65e27e5a6c
Mykyta Opanasiuk <nikitaopanassiuk@outlook.com>
2024-08-09 11:51:07 +0300

    Add additional fields types

--- src/types/weatherdata.ts
+++ src/types/weatherdata.ts
@@ -3,6 +3,7 @@ interface WeatherData {
     name: string;
     lat: number;
     lon: number;
+    tz_id: string;
   };
   current: {
     temp_c: number;
@@ -21,6 +22,50 @@ interface WeatherData {
       code: number;
     };
     is_day: boolean;
+  };
+  forecast: {
+    forecastday: [
+      {
+        date_epoch: number;
+        day: {
+          maxtemp_c: number;
+          mintemp_c: number;
+          avgtemp_c: number;
+          maxwind_kph: number;
+          totalprecip_mm: number;
+          avghumidity: number;
+          daily_will_it_rain: number;
+          daily_chance_of_rain: string;
+          daily_will_it_snow: number;
+          daily_chance_of_snow: string;
+          condition: {
+            text: string;
+            code: number;
+          };
+        };
+        hour: [
+          {
+            time_epoch: number;
+            temp_c: number;
+            is_day: boolean;
+            condition: {
+              text: string;
+              code: number;
+            };
+            wind_kph: number;
+            wind_degree: number;
+            wind_dir: string;
+            pressure_mb: number;
+            precip_mm: number;
+            humidity: number;
+            cloud: number;
+            feelslike_c: number;
+            dewpoint_c: number;
+            uv: number;
+          }
+        ];
+      }
+    ];
   };
 }
 
